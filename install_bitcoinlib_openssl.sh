3094ddb3e00e32331af632ca9920900f6deb23be
simewu <swuthier@uccs.edu>
2021-04-09 15:27:54 -0600

    Fix install script

--- install_bitcoinlib_openssl.sh
+++ install_bitcoinlib_openssl.sh
@@ -4,6 +4,7 @@ sudo apt install cmake gcc libtool libssl-dev make ninja-build git python3-pytes
 sudo rm -r openssl
 sudo rm -r liboqs
 
+
 git clone https://github.com/open-quantum-safe/liboqs.git
 git clone https://github.com/prchander/openssl.git
 OPENSSL_DIR=$PWD/openssl
@@ -20,3 +21,15 @@ cd $OPENSSL_DIR
 #./Configure no-shared linux-x86_64 -lm
 ./Configure shared linux-x86_64 -lm
 sudo make -j
+
+
+# Install python-bitcoinlib
+cd ~
+if [ ! -d "python-bitcoinlib" ] 
+then
+    git clone https://github.com/prchander/python-bitcoinlib.git
+fi
+
+cd python-bitcoinlib
+sudo python3 setup.py install
+export LD_LIBRARY_PATH=$HOME/openssl
